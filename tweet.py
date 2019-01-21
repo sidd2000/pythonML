2a26a78ad1ebd8431eb3cc63b1cbf17433ac3e81
sidd2000 <2016cssiddhant4682@poornima.edu.in>
2019-01-21 04:35:34 -0800

    tweet without code

--- tweet.py
+++ tweet.py
@@ -4,10 +4,10 @@ import tweepy
 
 
 # consumer keys and access tokens, used for OAuth
-consumer_key = '02rSqnC65ivFJqRCB2G69ktwh'
-consumer_secret = 'p1No1NFSDmN2zxovEr9wyxLuCWWkn2EKvGQMJ4nKWheEw5noKO'
-access_token = '889506304225665024-jgmCLgzDB42gO90OfsQ5eqiUPZP1JPm'
-access_token_secret = 'q8wJpvCc2M3SvowHTMtDrKXHJx7zjDrfX7cBLxrYypdpp'
+consumer_key = ''
+consumer_secret = ''
+access_token = ''
+access_token_secret = ''
 
 # OAuth process, using the keys and tokens
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
